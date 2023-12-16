313306d0adc060135b79f4bef4d9ae1f985c10fc
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-12-16 15:20:53 -0500

    feat: add routes

--- src/src/app/layout.tsx
+++ src/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from './providers'
-import { Navbar } from '@/components/UI/Navbar'
+import { Navbar } from '@/components/ui/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- src/src/app/providers.tsx
+++ src/src/app/providers.tsx
@@ -1,14 +1,14 @@
 'use client'
 import { ReactNode } from 'react'
 import { UseInkProvider } from 'useink'
-import { RococoContractsTestnet, ShibuyaTestnet } from 'useink/chains'
+import { ShibuyaTestnet } from 'useink/chains'
 
 function Providers ({ children }: { children: ReactNode }) {
   return (
     <UseInkProvider
       config={{
         dappName: 'Flipper',
-        chains: [RococoContractsTestnet, ShibuyaTestnet]
+        chains: [ShibuyaTestnet]
       }}
     >
       {children}
